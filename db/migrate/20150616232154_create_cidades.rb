c32ecb9e1d27a1c8fbcf62a94e84b268f212f9c0
Tiago Heineck <tiagovda@gmail.com>
2015-06-16 23:23:17 +0000

    Mudanças nas pastas

--- db/migrate/20150616232154_create_cidades.rb
+++ db/migrate/20150616232154_create_cidades.rb
@@ -1,9 +1,6 @@
 class CreateCidades < ActiveRecord::Migration
   def change
     create_table :cidades do |t|
-      t.primary_key :id
-      t.string :nome
-      t.string :estado
 
       t.timestamps null: false
     end
--- db/migrate/20150616232206_create_racas.rb
+++ db/migrate/20150616232206_create_racas.rb
@@ -1,8 +1,6 @@
 class CreateRacas < ActiveRecord::Migration
   def change
     create_table :racas do |t|
-      t.primary_key :id
-      t.string :nome
 
       t.timestamps null: false
     end
