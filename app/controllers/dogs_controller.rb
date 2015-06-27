6f5587b5f859bfe9a65df884553455a8c2b2c52a
Tiago Heineck <tiagovda@gmail.com>
2015-06-27 16:48:09 +0000

    Finalizando o fluxo com as validações do Facebook Closed #18

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -3,10 +3,16 @@ class DogsController < ApplicationController
   skip_before_filter :require_cadastro_completo
   
   def new
-    
+    @cidades = Cidade.all.collect {|c| [c.nome,c.id]}
+    @racas = Raca.all.collect {|c| [c.nome,c.id]}
   end
 
   def create
+    @dog =  Dog.new(dog_params)
+    @dog.dono_id = @current_user.id
+    @dog.save
+    flash[:notice] = "#{@dog.nome} foi cadastrado com sucesso."
+    redirect_to "/dogs"
   end
 
   def update
@@ -23,5 +29,10 @@ class DogsController < ApplicationController
   end
 
   def show
+  end
+  
+  private
+  def dog_params
+    params.required(:dog).permit(:nome,:sexo,:raca_id,:cidade_id,:descricao_perfil,:tem_pedigree,:interessa_cruzar)
   end
 end
