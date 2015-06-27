20389705bbe9d812e61fe2a07a44838ab7e737ff
Tiago Heineck <tiagovda@gmail.com>
2015-06-27 03:21:43 +0000

    Correções de bug #18

--- app/controllers/perfil_controller.rb
+++ app/controllers/perfil_controller.rb
@@ -7,19 +7,21 @@ class PerfilController < ApplicationController
     end
     
     def salvar
+        if params[:foto]
         path = File.join(Rails.root + "public/images",params[:foto].original_filename)
-        File.open(path,"wb") do |f|
-           f.write(params[:foto].read) 
+            File.open(path,"wb") do |f|
+                f.write(params[:foto].read) 
+            end
+            if @current_user.foto == nil 
+                foto = Foto.new
+            else
+                foto = @current_user.foto
+            end
+            foto.url = params[:foto].original_filename
+            foto.descricao = "Foto do Perfil"
+            foto.save
+            @current_user.foto = foto
         end
-        if @current_user.foto == nil 
-            foto = Foto.new
-        else
-            foto = @current_user.foto
-        end
-        foto.url = params[:foto].original_filename
-        foto.descricao = "Foto do Perfil"
-        foto.save
-        @current_user.foto = foto
         @current_user.sexo = params[:sexo]
         @current_user.save
         redirect_to "/perfil"
