0709561e2cd9cbf9a6d1eeac2bc67015b7b2b420
Plinio Garcia <plinioagarcia@gmail.com>
2015-07-01 02:10:48 +0000

    #19 inserindo campo cidade na busca

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,46 +16,7 @@
 //= require turbolinks
 //= require owl.carousel
 //= require_tree .
-/*
-function handleFileSelect(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
 
-    var files = evt.dataTransfer.files; // FileList object.
-
-    // files is a FileList of File objects. List some properties.
-    var output = [];
-    for (var i = 0, f; f = files[i]; i++) {
-      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-                  f.size, ' bytes, last modified: ',
-                  f.lastModifiedDate.toLocaleDateString(), '</li>');
-                  
-                  // Only process image files.
-      if (!f.type.match('image.*')) {
-        continue;
-      }
-
-      var reader = new FileReader();
-
-      // Closure to capture the file information.
-      reader.onload = (function(theFile) {
-        return function(e) {
-          // Render thumbnail.
-          var span = document.createElement('span');
-          span.innerHTML = ['<img class="thumb" src="', e.target.result,
-                            '" title="', escape(theFile.name), '"/>'].join('');
-          document.getElementById('list').insertBefore(span, null);
-        };
-      })(f);
-
-      // Read in the image file as a data URL.
-      reader.readAsDataURL(f);
-    }
-    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-  }
-
-
-*/
   function handleFileSelect(evt) {
   var files;
   var output = [];
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    
+    @cidades = Cidade.all.collect {|c| [c.nome,c.id]}
   end
 
 
