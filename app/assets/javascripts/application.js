619899043b2e3108df32b23bce1a9d65a954d45e
Plinio Garcia <plinioagarcia@gmail.com>
2015-07-12 15:11:43 +0000

    #41 movimento para colocar a latida current no topo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -76,3 +76,27 @@
       
   }
   
+  $.fn.animatePrependTo = function(sel, speed) {
+    var $this = this,
+        newEle = $this.clone(true).prependTo(sel),
+        newPos = newEle.position();
+    newEle.hide();
+    $this.css('position', 'absolute').animate(newPos, speed, function() {
+        newEle.show();
+        $this.remove();
+    });
+    return newEle;
+};
+
+  $.fn.animateAppendTo = function(sel, speed) {
+    var $this = this,
+        newEle = $this.clone(true).appendTo(sel),
+        newPos = newEle.position();
+    newEle.hide();
+    $this.css('position', 'absolute').animate(newPos, speed, function() {
+        newEle.show();
+        $this.remove();
+    });
+    return newEle;
+};
+  
